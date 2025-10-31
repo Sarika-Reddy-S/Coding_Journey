6de6764c58fb106a87a7f46461c6e16576534695
Sarika Reddy Sathi <155606470+Sarika-Reddy-S@users.noreply.github.com>
2025-10-31 09:13:04 +0530

    Time: 3 ms (64.73%) | Memory: 12.5 MB (12.28%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
@@ -1,7 +1,8 @@
-class Solution:
-    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+class Solution(object):
+    def getSneakyNumbers(self, nums):
         a=[]
         for i in range(len(nums)):
             if nums[i] in nums[(i+1):]:
                 a.append(nums[i])
         return a
+        
