f78a4123250bec9ab7e7e41ff27612b687a480dc
Sarika Reddy Sathi <155606470+Sarika-Reddy-S@users.noreply.github.com>
2025-12-18 19:47:51 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (5.38%) - LeetSync

--- 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.py
+++ 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.py
@@ -1,16 +1,3 @@
 class Solution(object):
     def removeTrailingZeros(self, num):
-        ans = list(num)
-        n = len(ans) - 1
-        val = ''
-
-        # remove trailing zeros
-        while n >= 0 and ans[n] == '0':
-            n -= 1
-
-        # build reversed value
-        while n >= 0:
-            val += ans[n]
-            n -= 1
-
-        return val[::-1]
+        return num.rstrip('0')
