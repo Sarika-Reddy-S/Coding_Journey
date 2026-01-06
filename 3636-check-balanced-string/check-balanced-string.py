34abf7e8312af9ea344846b25c35f9347fe0e0ee
Sarika Reddy Sathi <155606470+Sarika-Reddy-S@users.noreply.github.com>
2026-01-06 19:53:07 +0530

    Time: 9 ms (29.77%) | Memory: 12.6 MB (12.98%) - LeetSync

--- 3636-check-balanced-string/check-balanced-string.py
+++ 3636-check-balanced-string/check-balanced-string.py
@@ -2,13 +2,10 @@ class Solution(object):
     def isBalanced(self, num):
         odd=[]
         evn=[]
-        flag=False
         for i in range(len(num)):
             if i%2==0:
                 odd.append(int(num[i]))
             else:
                 evn.append(int(num[i]))
-        if sum(odd)==sum(evn):
-            flag=True
-        return (flag)
+        return (sum(evn)==sum(odd))
 
