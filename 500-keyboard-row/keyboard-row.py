ce9d94728beab80fb29abd70a971c115cbcf3e81
Sarika Reddy Sathi <155606470+Sarika-Reddy-S@users.noreply.github.com>
2026-01-07 19:16:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (53.59%) - LeetSync

---
+++ 500-keyboard-row/keyboard-row.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findWords(self, words):
+        rows = [
+            set("qwertyuiop"),
+            set("asdfghjkl"),
+            set("zxcvbnm")
+        ]
+        ans=[]
+        for word in words:
+            w = set(word.lower())
+            for row in rows:
+                if w.issubset(row):
+                    ans.append(word)
+                    break
+        return ans
+
+        
