6db4a85b4a88bc0ff195d2bd05d5d176f43ce07b
Sarika Reddy Sathi <155606470+Sarika-Reddy-S@users.noreply.github.com>
2026-07-23 12:24:48 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (97.30%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,13 +1,11 @@
-class Solution:
+class Solution(object):
     def moveZeroes(self, nums):
-        pos = 0
-        
-        # Move non-zero elements forward
-        for num in nums:
-            if num != 0:
-                nums[pos] = num
-                pos += 1
-        
-        # Fill remaining positions with zero
-        for i in range(pos, len(nums)):
-            nums[i] = 0
+        stack=[]
+        lst1=[]
+        for i in nums:
+            if i!=0:
+                stack.append(i)
+            else:
+                lst1.append(i)
+        nums[:]=stack+lst1
+        return nums 
